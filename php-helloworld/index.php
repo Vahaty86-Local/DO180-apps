204bc25814437bc3f8118c9d54382f16011895cf
Vahaty86-Local <55888955+Vahaty86-Local@users.noreply.github.com>
2021-01-24 11:21:13 +0100

    Update index.php
    fix it

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,3 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
-A change is in the air!
+print "Hello, World! php version is " . PHP_VERSION . "A change is in the air!" . . "\n";
 ?>
